06780bc7eb43769d3c990805a82a348c262a4114
ivaylo1070 <42966054+ivaylo1070@users.noreply.github.com>
2019-12-03 17:33:24 +0100

    Starting doing actual work here probably

--- Model/volunteer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$volunteer_Name
-$volunteer_Password
-$volunteer_Email
-$volunteer_PhoneNumber
-$volunteer_BirthDate
-
- ?>
---
+++ UI/usr_prof.php
@@ -0,0 +1,19 @@
+<?php
+include('Model/session.php');
+if(!isset($_SESSION['login_user'])){
+header("location: usr_prof.php"); // Redirecting To Home Page
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Your Home Page</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="profile">
+<b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
+<b id="logout"><a href="logout.php">Log Out</a></b>
+</div>
+</body>
+</html>
