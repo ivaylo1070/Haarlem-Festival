611ee390e25b6ca9eaefe124b42e04401d68a9b1
munatsimike <42965915+munatsimike@users.noreply.github.com>
2020-01-18 00:55:28 +0100

    Create index.php

---
+++ michael/index.php
@@ -0,0 +1,34 @@
+<?php
+require 'cart.php';
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>home</title>
+<link rel="stylesheet" type="text/css" href="css/mystyle.css"/>
+</head>
+<body>
+<h1>HAARLEM FESTIVAL JAZZ PROGRAM </h1>
+	<form>
+			<p>
+			<label for="">Sort by</label>
+	    <input type="radio"name="sort" checked>
+	    <label for="date"> Date </label>
+	    <input type="radio"name="sort">
+	    <label for="artist">Artist</label>
+		</p>
+	</form>
+	<form class="" action="cart.php" method="post">
+		<a href="cart.php">view cart</a>
+	</form>
+<table>
+
+<?php
+	//Display_Cart_Items();
+	DisplayTickets();
+?>
+
+</table>
+</body>
+</html>
