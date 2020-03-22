e12bb025862912c14f8f9156e9e6b83681544697
soumiabouhouri <bouhourisoumia@gmail.com>
2020-03-22 21:56:22 +0100

    ok

--- DAL/Cart_dal.php
+++ DAL/Cart_dal.php
@@ -4,6 +4,8 @@ include_once("config.php");
 include_once("dance_dal.php");
 include_once("jazz_dal.php");
 include_once("Reservation_dal.php");
+include_once("restaurant_dal.php");
+
 $dbconn = new config();
 $conn = $dbconn->Connect();
 abstract class cart_dal{
