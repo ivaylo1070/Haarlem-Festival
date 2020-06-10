ce3e3e8dbcf908d690e87d43057f6a1d75ec0786
soumiabouhouri <bouhourisoumia@gmail.com>
2020-06-10 22:49:07 +0200

    ui

--- UI/header.php
+++ UI/header.php
@@ -5,7 +5,7 @@
 <ul>
 	<li><a href="">EN</a></li>
 	<li><a href="<?php echo $link_address;?>" class="cartIcon" ></a></li>
-	<li><a href="<?php echo $link_address;?>Food_UI.php">Haarlem Food</a></li>
+	<li><a href="<?php echo $link_address;?>UI/Food_UI.php">Haarlem Food</a></li>
 	<li><a href="<?php echo $link_address;?>UI/Dance_UI.php">Haarlem Dance</a></li>
 	<li><a href="<?php echo $link_address;?>UI/Jazz_UI.php">Haarlem Jazz</a></li>
 	<li><a href="<?php echo $link_address;?>index.php">Home</a></li>
